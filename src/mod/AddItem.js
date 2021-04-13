f692643c64d4bb29ac2b2d3c7a715903fd12d56f
mohammad-adil <shahaadil13@gmail.com>
2021-04-13 11:03:24 +0530

    Added route for Add Item'

--- src/mod/AddItem.js
+++ src/mod/AddItem.js
@@ -7,16 +7,26 @@ const router = new express.Router()
 let AddItem = async(req,res,next)=>{
 let ItemId = await GenerateId(req.body.ITEM_NAME)
 console.log(ItemId)
-
-
-
+let results = await saveItem(req.body, ItemId)
 
 };
 
 
+// Generate Ids for the items that has to be created on the go..
 let GenerateId = async function(name){
   return name.substr(0,4) + Math.random().toString(36).substr(8, 9);
 }
+
+//. Saving Item to database with the detail
+let  saveItem = async function(Item,Itemid){
+let sql = "INSERT INTO assets (ITEM_NAME, TOTAL_QUANTITY, SUPPLIED_BY, ENTRY_BY, DEPARTMENT, LAB_ID, CATEGORY, ITEM_TYPE, UNIT_PRICE, ITEM_ID, SOURCE, AVL_QUANTITY) VALUES('"+ Item.ITEM_NAME +"','"+ Item.TOTAL_QUANTITY +"', '"+ Item.SUPPLIED_BY +"', '"+ Item.ENTRY_BY +"', '"+ Item.DEPARTMENT +"', '"+ Item.LAB_ID +"', '"+ Item.CATEGORY +"', '"+ Item.ITEM_TYPE +"', '"+ Item.UNIT_PRICE +"', '"+ Itemid +"','"+ Item.SOURCE +"','"+ Item.TOTAL_QUANTITY +"')"
+
+
+   db.query(sql, function (err, result) {console.log(result); console.log(err)});
+
+}
+
+
 
 
 
