038c858a3d7a31afd3898979aa4c984830e73021
Sheikh Rasik Lawlakh <71372042+sheikhlaulaq@users.noreply.github.com>
2021-04-22 10:59:36 +0530

    Minor changes in validation
    There were few bugs in validation, corrected them.
    There still is one bug, I am looking into it.

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -7,7 +7,7 @@ $(document).on('click', '#signup', function() {
    let password = document.getElementById('#password').value
    let confirmpassword = document.getElementById('#confirmpassword').value
    let department = document.getElementById('#department').value
-   let id = document.getElementById('#id').value
+   let eid = document.getElementById('#eid').value
 
 let UserDetails ={
     name,
@@ -16,62 +16,52 @@ let UserDetails ={
     password,
     confirmpassword,
     department,
-    id
+    eid,
 }
 
 var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 var phoneformat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 if(UserDetails.name ==""){
     document.getElementById('Nameid').innerHTML= "**Please fill Name";
-    return false;
   }
 
-  else if(!isNaN(UserDetails.name)){
+ else if(!isNaN(UserDetails.name)){
     document.getElementById('Nameid').innerHTML= "**Please enter character";
-    return false;
   }
-  else if(UserDetails.email ==""){
+ else if(UserDetails.email ==""){
     document.getElementById('emailid').innerHTML= "**Please fill Email";
-    return false;
   }
  
-else if(!(UserDetails.email.match(mailformat))){
+ else if(!(UserDetails.email.match(mailformat))){
     document.getElementById('emailid').innerHTML="**Please provide a valid email address";
-    email.focus;
-    return false;
-    
-}
-  else if(USerDetails.id ==""){
-    document.getElementById('Empid').innerHTML= "**Please fill Employee ID";
-    return false
   }
-  else if(UserDetails.phone ==""){
+
+ else if(UserDetails.phone ==""){
     document.getElementById('phoneid').innerHTML= "**Please fill Phone Number";
-    return false;
   }
-  if(!(UserDetails.phone.match(phoneformat))){
+
+ else if(!(UserDetails.phone.match(phoneformat))){
 
     document.getElementById('phoneid').innerHTML="Please provide a valid phone number";
-    phone.focus;
-    return false;
-    
-}
-  else if(UserDetails.password ==""){
-    document.getElementById('passwordid').innerHTML= "**Please fill Password";
-    return false;
   }
 
-  else if((UserDetails.password.length<5)||(UserDetails.passsword.length>20)){
-    document.getElementById('passwordid').innerHTML= "**Please fill Password between 6 and 20";
-    return false;
+ else if(UserDetails.password =="" && ((UserDetails.password.length<5)||(UserDetails.passsword.length>20))){
+    document.getElementById('passwordid').innerHTML= "****Please fill Password between 6 and 20";
   }
-  else if(UserDetails.password!=UserDetails.confirmpassword){
+
+ else if(UserDetails.confirmpassword =="" && (!(UserDetails.password==UserDetails.confirmpassword))){
     document.getElementById('cpasswordid').innerHTML= "**Passwords do not match";
   }
-  else if(UserDetails.confirmpassword ==""){
-    document.getElementById('cpasswordid').innerHTML= "**Please fill Confirm Password";
+
+ else if(UserDetails.eid ==""){
+    document.getElementById('empid').innerHTML= "**Please fill Employee ID";
   }
-SendData(UserDetails);
+  else {
+    SendData(UserDetails);
+  }
+
+
+
 })
 
 
