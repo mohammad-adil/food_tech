6346951fffaed682a02ba330676bd43083b7fe2c
mohammad-adil <shahaadil13@gmail.com>
2021-08-03 14:11:46 +0530

    updates'

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/stockpile", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(
+    "mongodb+srv://stockpile:stockpile@stockpile.otqrj.mongodb.net/stockpile?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB server");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
