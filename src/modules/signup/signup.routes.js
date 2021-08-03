06b58dc0834fb68831bba6cbda1987cd65515797
mohammad-adil <shahaadil13@gmail.com>
2021-08-03 14:19:56 +0530

    updates'

--- src/modules/signup/signup.routes.js
+++ src/modules/signup/signup.routes.js
@@ -3,4 +3,6 @@ const _signupService = require("../signup/signup.services");
 const prefix = "/stockpile/v1";
 router.post(prefix + "/signUp", _signupService.doSignupUser, (req, res) => {});
 
+router.get(prefix + "/getUser", _signupService.doGetUser, (req, res) => {});
+
 module.exports = router;
