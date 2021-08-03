7f60f953d7f339535c7c2024ec5b3fa42d35b279
Sheikh Rasik Lawlakh <71372042+sheikhlaulaq@users.noreply.github.com>
2021-08-03 21:22:57 +0530

    All

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb://127.0.0.1:27017/stockpile",
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose
+  .connect(
+    "mongodb://127.0.0.1:27017/stockpile",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB server");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+//  "mongodb+srv://stockpile:stockpile@stockpile.otqrj.mongodb.net/stockpile?retryWrites=true&w=majority",
