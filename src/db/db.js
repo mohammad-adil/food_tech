dff425e55771f8879749c95cd099b783dfcf58b7
mohammad-adil <shahaadil13@gmail.com>
2021-08-03 13:04:11 +0530

    updates'

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://admin:admin@stockpile.otqrj.mongodb.net/StockPile?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect("mongodb://127.0.0.1:27017/stockpile", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
