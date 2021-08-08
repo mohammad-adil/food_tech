dfa49b72f593dc4c9166b79d8d3d394e2c971881
mohammad-adil <shahaadil13@gmail.com>
2021-08-08 09:52:09 +0530

    updates on routes

--- src/db/db.js
+++ src/db/db.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb://127.0.0.1:27017/stockpile",
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect("mongodb://127.0.0.1:27017/stockpile", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => {
     console.log("Connected to MongoDB server");
   })
