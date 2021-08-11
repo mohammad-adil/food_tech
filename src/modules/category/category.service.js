5eb83b46a370838bb3514c8490661ac13e21fcc9
mohammad-adil <shahaadil13@gmail.com>
2021-08-11 13:33:20 +0530

    updates'

---
+++ src/modules/category/category.service.js
@@ -0,0 +1,7 @@
+const Category = require("../category/category.model");
+const SubCategory = require("../category/subcategory.model");
+
+exports.doAddCategory = async (req, res, next) => {};
+exports.doAddSubCategory = async (req, res, next) => {};
+exports.doUpdateCategory = async (req, res, next) => {};
+exports.doUpdateSubCategory = async (req, res, next) => {};
